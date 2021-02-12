206c29f5f21601b35db0d083f982754abaf23393
desireefernandes <desireefernandes00@gmail.com>
2021-02-12 17:03:45 -0300

    Add ao dashboard listagem entidade tarefas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+                
+                @foreach ($user->tarefas as $tarefa)
+                
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <b>TAREFA</b> <br>
+                        Prazo: {{ $tarefa->finalizacao}}
+                        <br><br>
+                        Título: <i> {{ $tarefa->titulo }} </i> <br>
+                        Descrição: <i> {{ $tarefa->descricao}} </i>
+                    </div>
+
+                @endforeach
+
             </div>
         </div>
     </div>
